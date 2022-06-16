3c244388421daaa9a99cec7db907a44ee4edb164
Sabhack <mi.sabhack@gmail.com>
2022-06-16 11:57:42 +0100

    A function to create grid (div)

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function createGrid(number) {
+    const gridContainer = document.querySelector('.grid-container');
+    gridContainer.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+    for (let i = 1; i <= number * number; i++) {
+        let div = document.createElement('div');
+        div.classList.add('grid-box');
+        gridContainer.appendChild(div);
+    }
+    document.querySelector('.grid-dimensions').textContent = `${number}x${number}`;
+}
+
+function changeColor(event) {
+    event.target.style.backgroundColor = 'black';
+}
